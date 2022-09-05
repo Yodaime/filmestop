fdc910d8bd20d214f8341f3715b3a8de3d2fbd84
Odar Carvalho <odair.carvalho@seuconsumo.com.br>
2022-09-05 12:31:26 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> FilmesTop
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+        </form>
+      <button type="submit">
+        <BiSearchAlt2 />
+      </button>
+      <button type="submit">
+        <BiSearchAlt2 />
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
